f13e5aef1333a58ba96b56a57e6868522084e2f4
jhj951105@gmail.com <jhj951105@gmail.com>
2023-12-13 14:05:01 +0900

    Init project

---
+++ team5_toy/src/main/java/com/example/demo/Team5ToyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5ToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5ToyApplication.class, args);
+	}
+
+}
